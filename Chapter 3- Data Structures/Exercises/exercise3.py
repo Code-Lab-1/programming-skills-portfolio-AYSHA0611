60600409e06d5495cabf6b1f7745313c466af23f
Aysha <ayshanver06@gmail.com>
2023-03-11 12:04:27 +0400

    exercise3

---
+++ Chapter 3- Data Structures/Exercises/exercise3.py
@@ -0,0 +1,5 @@
+vehicle=["Car","Bike","Bus","Motorcycle"]
+print(vehicle[0],"I own a Nissan Car")
+print(vehicle[0],"I have bike ")
+print(vehicle[0],"I have a bus")
+print(vehicle[0],"I have Suzuki Motorcycle")
