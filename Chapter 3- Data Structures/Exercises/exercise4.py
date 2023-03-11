cf568a9d125898c7b2a5c18702f1ead85d5e41f2
Aysha <ayshanver06@gmail.com>
2023-03-11 12:19:37 +0400

    Create exercise4.py

---
+++ Chapter 3- Data Structures/Exercises/exercise4.py
@@ -0,0 +1,7 @@
+list=["Sharvari","Rachita","Vandana","Julisha","Abiya"]
+print(list[0],"You are invited to dinner ")
+print(list[1],"You are invited to dinner")
+print(list[2],"You are invited to dinner")
+print(list[3],"You are invited to dinner")
+print(list[4],"You are invited to dinner")
+
